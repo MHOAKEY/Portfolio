e8cd56210e564f0db9731d4bc1221f8b7b1d9562
Matt Oake <matt@Matts-MacBook-Air.local>
2023-03-06 15:38:24 -0400

    added alt info

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import Logo from "../images/DivaXMAS.jpeg";
 export default function Navbar() {
   return (
     <nav>
-      <img src={Logo} id="Logo" />
+      <img src={Logo} alt="logo" id="Logo" />
       <h3 className="nav--logoText">MattOake</h3>
       <h4 className="nav--title">Stuff Here - test 1</h4>
     </nav>

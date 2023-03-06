8532d7b6e59f839cf32f1b50d748cdf84a6226de
Matt Oake <matt@Matts-MacBook-Air.local>
2023-03-06 14:44:25 -0400

    class names / style

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,16 @@
 import React from "react";
 
 export default function Main() {
-  return <h1>Main component</h1>;
+  return (
+    <div className="main">
+      <h1 className="main--title">Fun things about me!</h1>
+      <ul className="main--facts">
+        <li>I don't enjoy ketchup</li>
+        <li>Cheezewiz mmmmmmm</li>
+        <li>I like to cook beacuse I love to eat</li>
+        <li>Learning new things is fun to me</li>
+        <li>I'm always drowning in projects</li>
+      </ul>
+    </div>
+  );
 }

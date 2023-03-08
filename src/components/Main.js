1326dc671604804ed7c3aa646570d5e8b73af8e7
Matt Oake <matt@Matts-MacBook-Air.local>
2023-03-08 17:49:05 -0400

    Main() add li tag with more info. fixed spelling of cheezwhiz

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,8 +6,9 @@ export default function Main() {
     <div className="main">
       <h1 className="main--title">Fun things about me!</h1>
       <ul className="main--facts">
+        <li>That's my dog Diva! She's a good gorl :3</li>
         <li>I don't enjoy ketchup</li>
-        <li>Cheezewiz mmmmmmm</li>
+        <li>Cheezwhiz mmmmmmm</li>
         <li>I like to cook beacuse I love to eat</li>
         <li>Learning new things is fun to me</li>
         <li>I'm always drowning in projects</li>

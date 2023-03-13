4c460a48fab8bab2d7968c550bc8777f4c51a75b
Matt Oake <matt@Matts-MacBook-Air.local>
2023-03-13 15:28:29 -0300

    more card work

--- src/data.js
+++ src/data.js
@@ -3,10 +3,20 @@ import pythonImg from "./images/PythonLogo.png";
 import pythonVscImg from "./images/PythonVSCODE.png";
 import htmlLogo from "./images/htmlLogo.png";
 import htmlVscode from "./images/htmlVscode.png";
+import githubLogo from "./images/githubLogo.png";
 
 export default [
   {
     id: 0,
+    title: "GitHub",
+    description: "something",
+    mainImg: githubLogo,
+    vscodeImg: githubLogo,
+    other: "something",
+    other2: "something",
+  },
+  {
+    id: 1,
     title: "Python",
     description: "something",
     mainImg: pythonImg,
@@ -15,7 +25,7 @@ export default [
     other2: "something",
   },
   {
-    id: 1,
+    id: 2,
     title: "HTML",
     description: "something",
     mainImg: htmlLogo,

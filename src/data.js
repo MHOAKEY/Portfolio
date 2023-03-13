2af1cd05a57298c72cfde0c07120d635d854fafd
Matt Oake <matt@Matts-MacBook-Air.local>
2023-03-13 15:20:56 -0300

    import html image and add info to data sheet

--- src/data.js
+++ src/data.js
@@ -1,6 +1,8 @@
 import React from "react";
 import pythonImg from "./images/PythonLogo.png";
 import pythonVscImg from "./images/PythonVSCODE.png";
+import htmlLogo from "./images/htmlLogo.png";
+import htmlVscode from "./images/htmlVscode.png";
 
 export default [
   {
@@ -9,6 +11,15 @@ export default [
     description: "something",
     mainImg: pythonImg,
     vscodeImg: pythonVscImg,
+    other: "something",
+    other2: "something",
+  },
+  {
+    id: 1,
+    title: "HTML",
+    description: "something",
+    mainImg: htmlLogo,
+    vscodeImg: htmlVscode,
     other: "something",
     other2: "something",
   },
